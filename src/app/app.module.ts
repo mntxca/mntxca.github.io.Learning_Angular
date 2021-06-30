aaec5138c31589c1fb7e304533a671ffd96f3402
Raj Kumar <rk185362@ncr.com>
2021-06-30 14:33:09 -0700

    This is the Angular Project I have been working on, not completed yet

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { convertToSpacesPipe } from './shared/convert-to-spaces-pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent,
+  ProductListComponent, convertToSpacesPipe, StarComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
